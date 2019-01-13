fd6f47317069b4d5d61cd677cdb50c896dbd11a6
Antonio José <chindelrio@gmail.com>
2019-01-13 11:08:33 +0100

    Create practicabucles2.js

---
+++ practicabucles2.js
@@ -0,0 +1,17 @@
+<script>
+
+for (i = 0; i < 50; i++) {
+	nume = Math.floor((Math.random() * 100) + 1); 
+    	if (nume % 2 == 1) {
+    		nume.push(numeimpar);
+    	}
+    	if (nume % 2 == 0) {
+    		nume.push(numepar);
+    	};
+}
+
+return {
+	"Impares": numeimpar , "pares": numepar
+    };
+
+</script>
